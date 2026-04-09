5008ad59eb6bc59418121972e6b975c5bb306d72
KillMidory <123518287+KillMidory@users.noreply.github.com>
2026-04-09 16:12:35 -0600

    [M3Step 1](feat)-> Implement DetailsPage Component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
     const navigate = useNavigate();
@@ -15,6 +16,7 @@ function App() {
         <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/app" element={<MainPage />} />
+            <Route path="/app/product/:productId" element={<DetailsPage />} />
             <Route path="/app/login" element={<LoginPage/>} />
             <Route path="/app/register" element={<RegisterPage />} />
         </Routes>
